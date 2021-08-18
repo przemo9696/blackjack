31321a5c13c263a2646188f5b9e4ce93c1cd718c
Przemek <przemek.iskra96@gmail.com>
2021-08-18 10:09:45 +0200

    Game.js update

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -6,23 +6,53 @@ const TheGame = () => {
 
     const [deck, setDeck] = useState(null);
     const [count, setCount] = useState(0);
-    let playerHand = []; 
+    const [playerHand] = useState([]);
 
     useEffect(() => {
         fetchDeck(setDeck);
     }, []);
 
-    const onChangeHandler = () => {
-        fetchCard(deck);
+    // rewrite using API's pile  // TU JEST BUG \/
+    
+    const onHitHandler = async () => {
+        let card = await fetchCard(deck);
+        playerHand.push(card);
+
+        if (card[0].value === 'ACE') {
+            setCount(count + 11);
+        }
+
+        else if (card[0].value === 'JACK' || card[0].value === 'QUEEN' || card[0].value === 'KING') {
+            setCount(count + 10);
+        }
+        else {
+            setCount(count + Number(card[0].value));
+        } 
+    };
+
+    const showCards = () => {
+        playerHand.map((card) => {
+            console.log(card[0].code);
+            return (
+                <h1>{card[0].code}</h1>
+            )
+        })
     }
 
     return (
         <div className="main">
+            <div>
+                {playerHand.map((card) => {
+                    return (
+                        <h1 key={card[0].code}>{card[0].code}</h1>
+                    )
+                })}
+            </div>
             <div className="user-interface-bg">6</div>
             <div className="user-interface">
-                <button className="user-button" onClick={onChangeHandler}>HIT</button>
+                <button className="user-button" onClick={onHitHandler}>HIT</button>
                 <div className="user-counter">{count}</div>
-                <button className="user-button">PASS</button>
+                <button className="user-button" onClick={showCards}>PASS</button>
             </div>
         </div>
     );
