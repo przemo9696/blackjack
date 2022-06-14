b6dedba9d2a38346e8b81a04a70b048bfabfdbc2
Przemek <przemek.iskra96@gmail.com>
2022-06-14 13:15:56 +0200

    Added Constants.js file

---
+++ src/components/Game/Constants.js
@@ -0,0 +1,26 @@
+export const HANDS = {
+    FIRST: 'FIRST',
+    SECOND: 'SECOND',
+}
+
+export const MESSAGES = {
+    BLACKJACK: 'Blackjack',
+    PLAYA_GAIN: 'Play Again',
+    WIN: 'You Won!',
+    LOSE: 'You Lost!',
+    DRAW: 'Draw!',
+    SPLIT: 'Split?',
+}
+
+export const FADE = {
+    IN: 'fade-in',
+    OUT: 'fade-out',
+}
+
+export const BUTTON ={
+    PLAY: 'PLAY',
+    YES: 'YES',
+    NO: 'NO',
+    HIT: 'HIT',
+    STAND: 'STAND',
+}
