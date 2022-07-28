cc39b4db3a31a92786f336338bf9e5b28adce13e
Przemek <przemek.iskra96@gmail.com>
2022-07-28 14:03:07 +0200

    replaced strings with variables

--- src/helpers/drawCard.js
+++ src/helpers/drawCard.js
@@ -1,10 +1,11 @@
-import {fetchCard} from '../components/Game/Fetch.js';
+import { CARD } from '../components/Game/Constants.js';
+import { fetchCard } from '../components/Game/Fetch.js';
 
 export const drawCard = async (handSetter, count, countSetter, deck, temporaryCount = 0) => {
     let card = await fetchCard(deck, 1);
     handSetter((oldHand) => [...oldHand, card])
-    
-    if (card[0].value === 'ACE') {
+
+    if (card[0].value === CARD.ACE) {
         if(count + temporaryCount + 11 <= 21) {
             countSetter(prevCount => prevCount + 11);
         }
@@ -12,7 +13,7 @@ export const drawCard = async (handSetter, count, countSetter, deck, temporaryCo
             countSetter(prevCount => prevCount + 1);
         }
     }
-    else if (card[0].value === 'JACK' || card[0].value === 'QUEEN' || card[0].value === 'KING') {
+    else if (card[0].value === CARD.JACK || card[0].value === CARD.QUEEN || card[0].value === CARD.KING) {
         countSetter(prevCount => prevCount + 10);
     }
     else {
